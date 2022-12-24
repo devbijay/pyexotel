318e2fcf74d054af6a00ea34df9041e1eb1392c5
Bijay Nayak <bijay6779@gmail.com>
2022-12-24 22:08:51 +0530

    Update Version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 DESCRIPTION = 'Unofficial Exotel SDK For Python'
 LONG_DESCRIPTION = 'A package that allows you to make call, send sms and manage campaigns using exotel API'
 
